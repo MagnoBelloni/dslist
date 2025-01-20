bdf887421d27c213eae78a020b4f0f2a5c4d7a15
Magno Belloni <bellonidesouza6@gmail.com>
2025-01-20 16:42:24 -0300

    List games logic

---
+++ src/main/java/com/example/dslist/service/GameService.java
@@ -0,0 +1,22 @@
+package com.example.dslist.service;
+
+import com.example.dslist.dto.GameMinDTO;
+import com.example.dslist.entities.Game;
+import com.example.dslist.repositories.GameRepository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+    @Autowired
+    private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findAll() {
+        List<Game> result = gameRepository.findAll();
+        return result.stream().map(GameMinDTO::new).toList();
+    }
+}
