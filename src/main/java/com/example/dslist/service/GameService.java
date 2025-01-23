d1b4035666f2e731e5b2f97a8ea14898bd04ee41
Magno Belloni <bellonidesouza6@gmail.com>
2025-01-23 13:56:43 -0300

    Dev enviroment configs

--- src/main/java/com/example/dslist/service/GameService.java
+++ src/main/java/com/example/dslist/service/GameService.java
@@ -3,6 +3,7 @@ package com.example.dslist.service;
 import com.example.dslist.dto.GameDTO;
 import com.example.dslist.dto.GameMinDTO;
 import com.example.dslist.entities.Game;
+import com.example.dslist.projections.GameMinProjection;
 import com.example.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,5 +26,11 @@ public class GameService {
     public GameDTO findById(Long listId) {
         Game result = gameRepository.findById(listId).get();
         return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByGameList(Long listId) {
+        List<GameMinProjection> games = gameRepository.searchByList(listId);
+        return games.stream().map(GameMinDTO::new).toList();
     }
 }
