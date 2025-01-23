a37ad9ac67511847d0f1bd9987b93e51572740a9
Magno Belloni <bellonidesouza6@gmail.com>
2025-01-23 14:15:58 -0300

    Fixed incompability with h2 and postgres query

--- src/main/java/com/example/dslist/dto/GameMinDTO.java
+++ src/main/java/com/example/dslist/dto/GameMinDTO.java
@@ -21,7 +21,7 @@ public class GameMinDTO {
     public GameMinDTO(GameMinProjection projection) {
         id = projection.getId();
         title = projection.getTitle();
-        year = projection.getYear();
+        year = projection.getgame_year();
         imgUrl = projection.getImgUrl();
         shortDescription = projection.getShortDescription();
     }
